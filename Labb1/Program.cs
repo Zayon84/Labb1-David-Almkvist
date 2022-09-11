7b98e512fd13b87d34f6663c5b492c1fdb68407a
Zayon84 <dalmkvist@hotmail.com>
2022-09-11 19:49:22 +0200

    added some comments

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -2,3 +2,28 @@
 // David Almvkvist
 
 string defaultStringForTest = "29535123p48723487597645723645";
+int sumOfAllNumbers = 0;
+
+
+
+// Loop all characters
+
+//check if its a number
+
+// loop to find next same number
+
+// Add to sum
+void AddToSum(int valueToAdd)
+{
+    sumOfAllNumbers += valueToAdd;
+}
+
+// Convert to int
+
+
+
+
+if (char.IsDigit(defaultStringForTest[2]))      // icke siffra hittad
+{
+
+}
